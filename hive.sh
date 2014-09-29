c0a038e9d568e5cdd23741c4ac5878b2c84d2ea5
Daniel Templeton <templedf@gmail.com>
2014-09-29 16:13:10 -0700

    Testing Hive fix script

--- hive.sh
+++ hive.sh
@@ -4,9 +4,13 @@ grep hive.aux.jars.path /usr/lib/hive/conf/hive-site.xml >/dev/null 2>&1
 
 if [ $?=1 ]; then
   sed -e '\#</configuration># i\
+\
   <property>\
     <name>hive.aux.jars.path</name>\
     <value>file:////usr/lib/hive/lib/hive-contrib.jar</value>\
     <description>Add the Hive contrib JAR to Hive</description>\
-  </property>'
+  </property>' < /usr/lib/hive/conf/hive-site.xml > /tmp/hive-site.xml
+  sudo rm -f /usr/lib/hive/conf/hive-site.xml
+  sudo mv /tmp/hive-site.xml /usr/lib/hive/conf
+  sudo chown root:root /usr/lib/hive/conf/hive-site.xml
 fi
