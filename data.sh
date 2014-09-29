aa103c120525824376bdac6d126eae0ef6a3d31a
Daniel Templeton <templedf@gmail.com>
2014-09-29 16:35:39 -0700

    Testing data fix script

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gunzip ~/javaone_handsonhadoop/data/users.csv.gz
-gunzip ~/javaone_handsonhadoop/data/logins.log.gz
+gunzip ~/javaone14_handsonhadoop/data/users.csv.gz
+gunzip ~/javaone14_handsonhadoop/data/logins.log.gz
