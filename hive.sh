0bfd2078b6bb3b7cba59a8fb8c9d041aa90a7bd6
Daniel Templeton <templedf@gmail.com>
2014-09-29 16:09:36 -0700

    Testing Hive fix script

--- hive.sh
+++ hive.sh
@@ -3,7 +3,7 @@
 grep hive.aux.jars.path /usr/lib/hive/conf/hive-site.xml >/dev/null 2>&1
 
 if [ $?=1 ]; then
-  sed -e '\#</properties># i\
+  sed -e '\#</configuration># i\
   <property>\
     <name>hive.aux.jars.path</name>\
     <value>file:////usr/lib/hive/lib/hive-contrib.jar</value>\
