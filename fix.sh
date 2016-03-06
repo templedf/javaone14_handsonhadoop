96ed0fa176e981c320a341f0b0cfd0897178d458
Daniel Templeton <daniel@cloudera.com>
2016-03-06 13:20:26 -0800

    Removed JavaOne references and updated for CDH5.5

--- fix.sh
+++ fix.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 ~/javaone14_handsonhadoop/pdf.sh
-~/javaone14_handsonhadoop/hive.sh
-~/javaone14_handsonhadoop/pig.sh
+~/javaone14_handsonhadoop/mvn.sh
 ~/javaone14_handsonhadoop/data.sh
--- mapreduce/src/main/java/handsonhadoop/HandsOnHadoop.java
+++ mapreduce/src/main/java/handsonhadoop/HandsOnHadoop.java
@@ -1,4 +1,4 @@
-package javaone;
+package handsonhadoop;
 
 import java.io.IOException;
 import java.util.HashSet;
